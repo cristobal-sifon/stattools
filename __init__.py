b48b310c8cb623b434986aad86ccc38ec024c409
Cristobal Sifon <sifon@astro.princeton.edu>
2017-09-19 08:56:07 -0400

    updated functions and version to 0.1.2

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ Statistical tools
 
 """
 
-__all__ = ('bootstrap', 'Cbi', 'contour_levels', 'corner',
-           'draw', 'jackknife', 'MAD', 'Sbi', 'sigmaclip')
-__version__ = '0.1.1'
+__all__ = ('bootstrap', 'Cbi', 'draw', 'jackknife', 'MAD',
+           'percentile_from_histogram', 'Sbi', 'sigmaclip', 'wstd')
+__version__ = '0.1.2'
