d29930421853efa1002d1d3db362452b3ec6e04c
Cristobal Sifon <sifon@astro.princeton.edu>
2017-12-21 23:46:21 -0500

    new structure

--- stattools/__init__.py
+++ stattools/__init__.py
@@ -2,7 +2,12 @@
 Statistical tools
 
 """
+from __future__ import absolute_import
+
+from .stattools import *
+
 
 __all__ = ('bootstrap', 'Cbi', 'draw', 'jackknife', 'MAD',
            'percentile_from_histogram', 'Sbi', 'sigmaclip', 'wstd')
 __version__ = '0.1.2'
+
